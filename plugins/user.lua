7b316ab8c3a272e41111350cec1be7d389136026
rami3l <rami3l@outlook.com>
2023-12-31 14:03:20 +0800

    feat(lang): add `lean` support

--- plugins/user.lua
+++ plugins/user.lua
@@ -19,4 +19,21 @@ return {
       }
     end,
   },
+
+  {
+    "Julian/lean.nvim",
+    event = { "BufReadPre *.lean", "BufNewFile *.lean" },
+
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "nvim-lua/plenary.nvim",
+    },
+
+    opts = {
+      lsp = {
+        on_attach = (require "astronvim.utils.lsp").on_attach,
+      },
+      mappings = true,
+    },
+  },
 }
