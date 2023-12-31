7a078b773ccb50e9b295e9648f26f94c8544d541
rami3l <rami3l@outlook.com>
2023-12-31 14:07:35 +0800

    feat: enable rich text on true color terminals

--- options.lua
+++ options.lua
@@ -2,11 +2,12 @@
 return {
   opt = {
     -- set to true or false etc.
-    relativenumber = true, -- sets vim.opt.relativenumber
-    number = true, -- sets vim.opt.number
-    spell = false, -- sets vim.opt.spell
-    signcolumn = "auto", -- sets vim.opt.signcolumn to auto
-    wrap = true, -- sets vim.opt.wrap
+    relativenumber = true,
+    number = true,
+    spell = false,
+    signcolumn = "auto",
+    wrap = true,
+    termguicolors = true, -- enable rich text
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
