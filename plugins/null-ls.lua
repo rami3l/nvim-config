ebcb8758cfa9e49ee2b76bad8b3a5e36635bb85e
rami3l <rami3l@outlook.com>
2024-01-03 00:47:50 +0800

    fix(lang/java): disable `clang_format` for formatting

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -9,8 +9,10 @@ return {
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
     config.sources = {
       -- Set a formatter
-      -- null_ls.builtins.formatting.stylua,
       -- null_ls.builtins.formatting.prettier,
+      null_ls.builtins.formatting.clang_format.with {
+        disabled_filetypes = { "java" },
+      },
       null_ls.builtins.formatting.swift_format,
       null_ls.builtins.formatting.swiftlint,
     }
