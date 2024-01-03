bae1d55f60d734cc2a41cc709e7918ff36ca7f20
rami3l <rami3l@outlook.com>
2024-01-03 08:43:05 +0800

    feat(explorer): enable `group_empty_dirs` esp. for Java projects

--- plugins/neo-tree.lua
+++ plugins/neo-tree.lua
@@ -2,9 +2,9 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
     filesystem = {
-      filtered_items = {
-        visible = true,
-      },
+      filtered_items = { visible = true },
+      group_empty_dirs = true,
+      scan_mode = "deep",
     },
   },
 }
