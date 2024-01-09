838b2f4608960a661de84c6c5dff757df79da09e
rami3l <rami3l@outlook.com>
2024-01-09 10:09:38 +0800

    feat(ide): add the `im-select` plugin

--- plugins/user.lua
+++ plugins/user.lua
@@ -8,6 +8,18 @@ return {
 
   -- add other plugins
   {
+    "keaising/im-select.nvim",
+    event = "User Astrofile",
+    config = function(_, opts)
+      if vim.fn.has("macunix") == 1 then
+        opts.default_command = "macism"
+        opts.default_im_select = "com.apple.keylayout.US"
+      end
+      require("im_select").setup(opts)
+    end,
+  },
+
+  {
     "nvim-treesitter/nvim-treesitter-context",
     event = "User Astrofile",
     opts = {
