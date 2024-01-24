4238df535a673d8d56a1f8b9cb4f28fd24ca3054
rami3l <rami3l@outlook.com>
2024-01-24 19:09:27 +0800

    feat(ide): enable `cspell` for spell checking

--- plugins/none-ls.lua
+++ plugins/none-ls.lua
@@ -18,7 +18,12 @@ return {
     -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
     -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
     opts.sources = {
-      -- Set a formatter
+      nls.builtins.diagnostics.cspell.with {
+        -- https://github.com/davidmh/cspell.nvim/issues/13
+        diagnostics_postprocess = function(diagnostic)
+          diagnostic.severity = vim.diagnostic.severity["INFO"]
+        end,
+      },
       nls.builtins.formatting.clang_format.with {
         disabled_filetypes = { "java" },
       },
