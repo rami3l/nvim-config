02804bf10f44948ad561fcd12dfc22cb4b8dcecb
rami3l <rami3l@outlook.com>
2024-02-01 10:57:07 +0800

    feat(neogit): update neogit icons

--- plugins/neogit.lua
+++ plugins/neogit.lua
@@ -3,15 +3,18 @@ return {
   keys = {
     { "<leader>gm", function() require("neogit").open() end, desc = "Neogit dispatch" },
   },
-  opts = function(_, opts)
-    return require("astronvim.utils").extend_tbl(opts, {
-      mappings = {
-        -- NOTE: `c-` should be lowercase to clash with the default keybinding.
-        finder = {
-          ["<c-j>"] = "Next",
-          ["<c-k>"] = "Previous",
-        },
+  opts = {
+    signs = {
+      -- { CLOSED, OPENED }
+      item = { ">", "v" },
+      section = { ">", "v" },
+    },
+    mappings = {
+      -- NOTE: `c-` should be lowercase to clash with the default keybinding.
+      finder = {
+        ["<c-j>"] = "Next",
+        ["<c-k>"] = "Previous",
       },
-    })
-  end,
+    },
+  },
 }
