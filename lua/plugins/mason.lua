883ef05231e1b34e4cff0d9ac171a9f8b446cdd8
rami3l <rami3l@outlook.com>
2024-02-01 22:51:31 +0800

    feat(ide): enable `cspell`

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -17,10 +17,8 @@ return {
     -- overrides `require("mason-null-ls").setup(...)`
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astrocore").list_insert_unique(
-        opts.ensure_installed
-        -- , "cspell"
-      )
+      opts.ensure_installed =
+        require("astrocore").list_insert_unique(opts.ensure_installed, "cspell")
     end,
   },
   {
--- lua/plugins/none_ls.lua
+++ lua/plugins/none_ls.lua
@@ -2,27 +2,20 @@
 ---@type LazySpec
 return {
   "nvimtools/none-ls.nvim",
-  dependencies = {
-    -- TODO: Reenable cspell.
-    -- "davidmh/cspell.nvim",
-  },
   opts = function(_, opts)
     -- config variable is the default configuration table for the setup function call
     local nls = require("null-ls")
-
-    opts.on_attach = require("astrolsp").on_attach
 
     -- Check supported formatters and linters
     -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
     -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
     opts.sources = {
-      -- require("cspell").code_actions,
-      -- require("cspell").diagnostics.with {
-      --   -- https://github.com/davidmh/cspell.nvim/issues/13
-      --   diagnostics_postprocess = function(diagnostic)
-      --     diagnostic.severity = vim.diagnostic.severity["INFO"]
-      --   end,
-      -- },
+      nls.builtins.diagnostics.cspell.with {
+        -- https://github.com/davidmh/cspell.nvim/issues/13
+        diagnostics_postprocess = function(diagnostic)
+          diagnostic.severity = vim.diagnostic.severity["INFO"]
+        end,
+      },
       nls.builtins.formatting.clang_format.with {
         disabled_filetypes = { "java" },
       },
