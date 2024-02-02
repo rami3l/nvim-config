5822713a385f5a8239db6f366ead73e8eaa0920f
rami3l <rami3l@outlook.com>
2024-02-02 21:18:00 +0800

    feat(resession): add mappings for loading dirsessions

---
+++ lua/plugins/resession.lua
@@ -0,0 +1,11 @@
+return {
+  "stevearc/resession.nvim",
+  keys = {
+    -- https://github.com/stevearc/resession.nvim/tree/master?tab=readme-ov-file#api
+    {
+      "<Leader>SF",
+      function() require("resession").load(nil, { dir = "dirsession" }) end,
+      desc = "Load a dirsession",
+    },
+  },
+}
