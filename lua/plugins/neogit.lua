13f16e16d695094e4a2d547ad2b8809a27a8c404
rami3l <rami3l@outlook.com>
2024-02-06 12:53:18 +0800

    feat(neogit): update display options

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -4,6 +4,8 @@ return {
     { "<Leader>gm", function() require("neogit").open() end, desc = "Neogit dispatch" },
   },
   opts = {
+    graph_style = "unicode",
+    sections = { recent = { folded = false } },
     signs = {
       -- { CLOSED, OPENED }
       item = { ">", "v" },
