785b854751af36d29b0537e8f63a9ea0517f77d3
rami3l <rami3l@outlook.com>
2024-02-14 11:52:06 +0800

    feat(resession): add mappings for deleting dirsessions

--- lua/plugins/resession.lua
+++ lua/plugins/resession.lua
@@ -2,10 +2,10 @@ return {
   "stevearc/resession.nvim",
   keys = {
     -- https://github.com/stevearc/resession.nvim/tree/master?tab=readme-ov-file#api
-    -- {
-    --   "<Leader>SF",
-    --   function() require("resession").load(nil, { dir = "dirsession" }) end,
-    --   desc = "Load a dirsession",
-    -- },
+    {
+      "<Leader>SD",
+      function() require("resession").delete(nil, { dir = "dirsession" }) end,
+      desc = "Delete a dirsession",
+    },
   },
 }
