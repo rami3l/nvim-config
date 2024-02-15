061bc8f8db9856cd8fad0469d1e32c5089124e25
rami3l <rami3l@outlook.com>
2024-02-15 21:44:09 +0800

    chore(deps): bump lockfile

--- lua/plugins/resession.lua
+++ lua/plugins/resession.lua
@@ -2,10 +2,10 @@ return {
   "stevearc/resession.nvim",
   keys = {
     -- https://github.com/stevearc/resession.nvim/tree/master?tab=readme-ov-file#api
-    {
-      "<Leader>SD",
-      function() require("resession").delete(nil, { dir = "dirsession" }) end,
-      desc = "Delete a dirsession",
-    },
+    -- {
+    --   "<Leader>SD",
+    --   function() require("resession").delete(nil, { dir = "dirsession" }) end,
+    --   desc = "Delete a dirsession",
+    -- },
   },
 }
