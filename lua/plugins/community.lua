e559f8deca823243671312f8db1a72bffdee230a
rami3l <rami3l@outlook.com>
2024-02-19 11:40:24 +0800

    feat(mappings): active mappings for `:VenvSelect` on Python

--- lua/plugins/community.lua
+++ lua/plugins/community.lua
@@ -32,11 +32,20 @@ return {
     },
   },
 
-  -- astrocommunity.pack.python
+  -- astrocommunity.pack.python*
   {
     "linux-cultist/venv-selector.nvim",
     opts = {
       name = { "venv", ".venv" },
+    },
+    keys = {
+      { "<Leader>lv", "<CMD>VenvSelect<CR>", desc = "Select Venv", ft = "python" },
+    },
+    dependencies = {
+      {
+        "AstroNvim/astrocore",
+        opts = function(_, opts) opts.mappings.n["<Leader>lv"] = false end,
+      },
     },
   },
 
