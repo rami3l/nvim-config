15269be0b46a08f1e82a90c22d021195b7406fb5
rami3l <rami3l@outlook.com>
2024-02-19 22:44:43 +0800

    fix(autopairs): disable insertion of parens on func name completion

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,4 +1,9 @@
 return {
   "windwp/nvim-autopairs",
+  config = function(_, opts)
+    local nap = require("nvim-autopairs")
+    nap.setup(opts)
+    if not require("astrocore").config.features.autopairs then nap.disable() end
+  end,
   opts = { enable_check_bracket_line = true },
 }
