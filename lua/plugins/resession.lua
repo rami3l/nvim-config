ad0d41e2f26fb09dc258bf5845797702c08d5159
rami3l <rami3l@outlook.com>
2024-02-20 10:33:57 +0800

    feat(resession): refine mappings for deleting dirsessions

--- lua/plugins/resession.lua
+++ lua/plugins/resession.lua
@@ -1,11 +1,26 @@
 return {
   "stevearc/resession.nvim",
+  dependencies = {
+    {
+      "AstroNvim/astrocore",
+      opts = function(_, opts)
+        local n = opts.mappings.n
+        n["<Leader>Sd"] = { desc = "Delete" }
+        n["<Leader>SD"] = false
+      end,
+    },
+  },
   keys = {
     -- https://github.com/stevearc/resession.nvim/tree/master?tab=readme-ov-file#api
-    -- {
-    --   "<Leader>SD",
-    --   function() require("resession").delete(nil, { dir = "dirsession" }) end,
-    --   desc = "Delete a dirsession",
-    -- },
+    {
+      "<Leader>Sdf",
+      function() require("resession").delete() end,
+      desc = "Delete a session",
+    },
+    {
+      "<Leader>SdF",
+      function() require("resession").delete(nil, { dir = "dirsession" }) end,
+      desc = "Delete a dirsession",
+    },
   },
 }
