1ce1b926b1452447543b3c2ea06895124feb6ab0
rami3l <rami3l@outlook.com>
2024-03-31 16:29:20 +0800

    feat(ide/lang): add support for `fish`

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -38,6 +38,7 @@ lazy.setup({
   { import = "astrocommunity.motion.leap-nvim" },
   { import = "astrocommunity.motion.nvim-spider" },
   { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.pack.fish" },
   { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.pack.java" },
   { import = "astrocommunity.pack.json" },
