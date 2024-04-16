8129c542980694515d6d9e3ad9b544e70efaf7df
rami3l <rami3l@outlook.com>
2024-04-16 10:59:44 +0800

    feat(ide/cspell): work around cspell plugin installation

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -66,13 +66,10 @@ return {
     dependencies = {
       {
         "jay-babu/mason-null-ls.nvim",
-
         opts = function(_, opts)
           opts.ensure_installed =
             require("astrocore").list_insert_unique(opts.ensure_installed, { "cspell" })
-
           local cspell_cfg = { config_file_preferred_name = ".cspell.json" }
-
           opts.handlers.cspell = function()
             require("null-ls").register {
               require("cspell").code_actions.with { config = cspell_cfg },
@@ -88,5 +85,28 @@ return {
         end,
       },
     },
+    -- HACK: Works around <https://github.com/williamboman/mason.nvim/issues/392>.
+    config = function()
+      local astrocore = require("astrocore")
+      require("mason-registry"):on(
+        "package:install:success",
+        vim.schedule_wrap(function(
+          pkg --[[@as Package]]
+        )
+          if pkg.name == "cspell" then
+            local plugins = { "@cspell/dict-fr-fr" }
+
+            local base_path =
+              vim.fn.resolve(vim.fn.stdpath("data") .. "/mason/packages/cspell/node_modules/cspell")
+            for _, plugin in ipairs(plugins) do
+              astrocore.notify("Installing cspell plugin `" .. plugin .. "`")
+              astrocore.cmd({ "npm", "--prefix", base_path, "install", plugin }, true)
+              local ext_path = base_path .. "/node_modules/" .. plugin .. "/cspell-ext.json"
+              astrocore.cmd({ "cspell", "link", "add", ext_path }, true)
+            end
+          end
+        end)
+      )
+    end,
   },
 }
