83536a4c8eb4f2260baf0ac861379624a3152633
rami3l <rami3l@outlook.com>
2024-05-15 22:53:13 +0800

    feat(lang/typst): use `tinymist` instead of `typst_lsp` (again)

--- lua/plugins/community.lua
+++ lua/plugins/community.lua
@@ -145,6 +145,15 @@ return {
 
   -- astrocommunity.pack.typst
   {
+    "williamboman/mason-lspconfig.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(
+        vim.iter(opts.ensure_installed):filter(function(it) return it ~= "typst_lsp" end):totable(),
+        { "tinymist" }
+      )
+    end,
+  },
+  {
     "jay-babu/mason-null-ls.nvim",
     opts = function(_, opts)
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
