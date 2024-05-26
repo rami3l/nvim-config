cd337704321b6d08825661856ff449039c0db675
rami3l <rami3l@outlook.com>
2024-05-26 19:48:49 +0800

    feat(ide): adjust keybindings for `smart_splits`

--- lua/plugins/smart_splits.lua
+++ lua/plugins/smart_splits.lua
@@ -6,25 +6,42 @@ return {
       "AstroNvim/astrocore",
       opts = function(_, opts)
         local n = opts.mappings.n
-        n["<CA-Up>"] = { require("smart-splits").resize_up, desc = "Resize split up" }
-        n["<CA-Down>"] = { require("smart-splits").resize_down, desc = "Resize split down" }
-        n["<CA-Left>"] = { require("smart-splits").resize_left, desc = "Resize split left" }
-        n["<CA-Right>"] = { require("smart-splits").resize_right, desc = "Resize split right" }
+
+        local smart_splits = require("smart-splits")
+
+        n["<A-k>"] = { smart_splits.resize_up, desc = "Resize split up" }
+        n["<A-j>"] = { smart_splits.resize_down, desc = "Resize split down" }
+        n["<A-h>"] = { smart_splits.resize_left, desc = "Resize split left" }
+        n["<A-l>"] = { smart_splits.resize_right, desc = "Resize split right" }
 
         ---@param direction "up"|"down"|"left"|"right"
-        ---@return fun(): nil
-        local function smart_splits_swap_buf_and_focus(direction)
-          local smart_splits = require("smart-splits")
-          return function()
-            smart_splits["swap_buf_" .. direction]()
-            smart_splits["move_cursor_" .. direction]()
-          end
+        local function swap_split_entry(direction)
+          return {
+            function()
+              smart_splits["swap_buf_" .. direction]()
+              smart_splits["move_cursor_" .. direction]()
+            end,
+            desc = "Swap split " .. direction,
+          }
         end
 
-        n["<C-w><C-k>"] = { smart_splits_swap_buf_and_focus("up"), desc = "Swap split up" }
-        n["<C-w><C-j>"] = { smart_splits_swap_buf_and_focus("down"), desc = "Swap split down" }
-        n["<C-w><C-h>"] = { smart_splits_swap_buf_and_focus("left"), desc = "Swap split left" }
-        n["<C-w><C-l>"] = { smart_splits_swap_buf_and_focus("right"), desc = "Swap split right" }
+        local function hide_entry(entry)
+          return require("astrocore").extend_tbl(entry, { desc = "which_key_ignore" })
+        end
+
+        local swap_split_up = swap_split_entry("up")
+        local swap_split_down = swap_split_entry("down")
+        local swap_split_left = swap_split_entry("left")
+        local swap_split_right = swap_split_entry("right")
+
+        n["<CA-k>"], n["<C-w>K"], n["<C-w><CS-k>"] =
+          swap_split_up, swap_split_up, hide_entry(swap_split_up)
+        n["<CA-j>"], n["<C-w>J"], n["<C-w><CS-j>"] =
+          swap_split_down, swap_split_down, hide_entry(swap_split_down)
+        n["<CA-h>"], n["<C-w>H"], n["<C-w><CS-h>"] =
+          swap_split_left, swap_split_left, hide_entry(swap_split_left)
+        n["<CA-l>"], n["<C-w>L"], n["<C-w><CS-l>"] =
+          swap_split_right, swap_split_right, hide_entry(swap_split_right)
       end,
     },
   },
