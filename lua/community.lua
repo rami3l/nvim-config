317f6be4b8e2275d426e50dba0ddb225ea698ba7
rami3l <rami3l@outlook.com>
2024-06-06 09:39:28 +0800

    feat(lang/elixir): add the `elixir-phoenix` pack

--- lua/community.lua
+++ lua/community.lua
@@ -28,6 +28,7 @@ return {
   { import = "astrocommunity.motion.leap-nvim" },
   { import = "astrocommunity.motion.nvim-spider" },
   { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.pack.elixir-phoenix" },
   { import = "astrocommunity.pack.fish" },
   { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.pack.java" },
