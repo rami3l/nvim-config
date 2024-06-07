4fef67f3091a9f39fbe636d41cbd49d3211d3a62
rami3l <rami3l@outlook.com>
2024-06-07 10:38:51 +0800

    feat(lang/elixir): introduce `credo`

--- lua/plugins/community.lua
+++ lua/plugins/community.lua
@@ -92,6 +92,19 @@ return {
     },
   },
 
+  -- astrocommunity.pack.elixir
+  {
+    "nvimtools/none-ls.nvim",
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      if not opts.sources then opts.sources = {} end
+      opts.sources = vim.list_extend(opts.sources, {
+        nls.builtins.diagnostics.credo,
+      })
+      return opts
+    end,
+  },
+
   -- astrocommunity.pack.go
   {
     "williamboman/mason-lspconfig.nvim",
