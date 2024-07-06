8c3d4e7dec01c8a978eb469b2dcaffa13d9fbbb8
rami3l <rami3l@outlook.com>
2024-07-06 08:57:52 +0800

    chore(deps): bump lockfile

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -2,7 +2,7 @@
 return {
   "goolord/alpha-nvim",
   opts = function(_, opts)
-    opts.section.header.val = require("plugins.alpha.saturn")
+    opts.section.header.val = require("plugins.alpha.octahedron")
     return opts
   end,
 }
