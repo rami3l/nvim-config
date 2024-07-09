6060eb55a9b2a6d744ceadd1725b4b6eacf93e2b
rami3l <rami3l@outlook.com>
2024-07-09 10:03:31 +0800

    feat(ide): introduce `origami`

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -6,6 +6,8 @@ return {
   -- Add new user plugins below.
 
   -- UX enhancements
+  { "chrisgrieser/nvim-origami", event = "BufReadPost", opts = {} },
+
   {
     "mizlan/iswap.nvim",
     event = "VeryLazy",
