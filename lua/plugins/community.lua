1e5a1adb56f02dbed516297c8e701e31f1d16b92
rami3l <rami3l@outlook.com>
2024-07-10 10:42:11 +0800

    feat(ide): adjust mappings for `spectre`

--- lua/plugins/community.lua
+++ lua/plugins/community.lua
@@ -216,6 +216,25 @@ return {
         end,
       },
     },
+    opts = function()
+      local prefix = "<Leader>s"
+      return {
+        mapping = {
+          -- Override defaults
+          select_template = { map = prefix .. "p" },
+          delete_line = { map = prefix .. "d" },
+
+          -- Override `astrocommunity` presets
+          send_to_qf = { map = prefix .. "q" },
+          replace_cmd = { map = prefix .. "c" },
+          show_option_menu = { map = prefix .. "o" },
+          run_current_replace = { map = prefix .. "C" },
+          run_replace = { map = prefix .. "R" },
+          change_view_mode = { map = prefix .. "v" },
+          resume_last_search = { map = prefix .. "l" },
+        },
+      }
+    end,
   },
 
   -- astrocommunity.test.neotest
