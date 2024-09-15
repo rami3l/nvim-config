a616b8c619fa087d11d25ad92c071246fab2a80a
rami3l <rami3l@outlook.com>
2024-09-15 21:24:19 +0800

    fix(toggleterm)!: unset default direction

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -4,7 +4,5 @@ return {
     { "<Leader>!", '<Cmd>execute v:count . "ToggleTerm"<CR>', desc = "Toggle terminal" },
     { "<Leader>t<Enter>", "<Cmd>TermSelect<CR>", desc = "ToggleTerm select" },
   },
-  opts = {
-    direction = "float",
-  },
+  opts = {},
 }
