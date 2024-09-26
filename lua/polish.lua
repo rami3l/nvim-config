5e62d87bf5b8cf71998ba918715cf8fbbc963734
rami3l <rami3l@outlook.com>
2024-09-26 16:18:00 +0800

    feat(command): auto-correct `:W*` to `:w*`, and `:Q` to `:q`

--- lua/polish.lua
+++ lua/polish.lua
@@ -2,6 +2,15 @@
 -- augroups/autocommands and custom filetypes also this just pure lua so
 -- anything that doesn't fit in the normal config locations above can go here
 
+-- Auto-correct `:W*` to `:w*`, and `:Q` to `:q`.
+-- See <https://stackoverflow.com/a/10590421>.
+vim.api.nvim_create_user_command("W", "w", { bang = true })
+vim.api.nvim_create_user_command("Wa", "wa", { bang = true })
+vim.api.nvim_create_user_command("Wq", "wq", { bang = true })
+vim.api.nvim_create_user_command("Wqa", "wqa", { bang = true })
+vim.api.nvim_create_user_command("Q", "q", { bang = true })
+vim.api.nvim_create_user_command("Qa", "qa", { bang = true })
+
 -- Set up custom filetypes
 -- vim.filetype.add {
 --   extension = {
