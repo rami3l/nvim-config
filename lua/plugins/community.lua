cf85ca41b0616e9742bceb551320a30fd0815cba
rami3l <rami3l@outlook.com>
2024-10-18 21:41:14 +0800

    feat(lang): add `clojure`

--- lua/plugins/community.lua
+++ lua/plugins/community.lua
@@ -108,6 +108,16 @@ return {
     },
   },
 
+  -- astrocommunity.pack.clojure
+  {
+    "jay-babu/mason-null-ls.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+        "zprint",
+      })
+    end,
+  },
+
   -- astrocommunity.pack.elixir
   {
     "nvimtools/none-ls.nvim",
