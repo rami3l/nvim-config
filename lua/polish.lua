910f910334a63f6561e34e2e0a738f3738b6b818
rami3l <rami3l@outlook.com>
2024-11-09 14:49:38 +0800

    hack(lsp): work around https://github.com/neovim/neovim/issues/30985

--- lua/polish.lua
+++ lua/polish.lua
@@ -23,3 +23,12 @@ vim.api.nvim_create_user_command("Qa", "qa", { bang = true })
 --     ["~/%.config/foo/.*"] = "fooscript",
 --   },
 -- }
+
+-- HACK: Works around <https://github.com/neovim/neovim/issues/30985>.
+for _, method in ipairs { "textDocument/diagnostic", "workspace/diagnostic" } do
+  local default_diagnostic_handler = vim.lsp.handlers[method]
+  vim.lsp.handlers[method] = function(err, result, context, config)
+    if err ~= nil and err.code == -32802 then return end
+    return default_diagnostic_handler(err, result, context, config)
+  end
+end
