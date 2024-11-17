d7df5991b8328fe466b8c1d96dcac141ac8e45be
rami3l <rami3l@outlook.com>
2024-11-17 16:12:49 +0800

    feat(ide/neo-tree): use `D` to `trash` files/dirs

--- lua/plugins/neo_tree.lua
+++ lua/plugins/neo_tree.lua
@@ -1,11 +1,34 @@
 ---@type LazySpec
 return {
   "nvim-neo-tree/neo-tree.nvim",
-  opts = {
-    filesystem = {
-      filtered_items = { visible = true },
-      group_empty_dirs = true,
-      scan_mode = "deep",
-    },
-  },
+  opts = function(_, opts)
+    opts = vim.tbl_deep_extend("force", opts, {
+      filesystem = {
+        filtered_items = { visible = true },
+        group_empty_dirs = true,
+        scan_mode = "deep",
+      },
+    })
+    if vim.fn.executable("trash") == 1 then
+      opts = vim.tbl_deep_extend("force", opts, {
+        filesystem = {
+          window = { mappings = { D = "trash" } },
+          commands = {
+            trash = function(state)
+              local inputs = require("neo-tree.ui.inputs")
+              local path = state.tree:get_node().path
+              local _, name = require("neo-tree.utils").split_path(path)
+              local msg = string.format("Are you sure you want to trash '%s'?", name)
+              inputs.confirm(msg, function(confirmed)
+                if not confirmed then return end
+                vim.fn.system { "trash", path }
+                require("neo-tree.sources.manager").refresh(state.name)
+              end)
+            end,
+          },
+        },
+      })
+    end
+    return opts
+  end,
 }
