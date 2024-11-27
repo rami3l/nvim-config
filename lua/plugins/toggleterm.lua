0421002165215d28ced2e97424b35e1a66b3267a
rami3l <rami3l@outlook.com>
2024-11-27 16:14:44 +0800

    feat(ide/toggleterm): set default direction to `float`

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -4,5 +4,5 @@ return {
     { "<Leader>!", '<Cmd>execute v:count . "ToggleTerm"<CR>', desc = "Toggle terminal" },
     { "<Leader>t<Enter>", "<Cmd>TermSelect<CR>", desc = "ToggleTerm select" },
   },
-  opts = {},
+  opts = { direction = "float" },
 }
