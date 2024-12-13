4ea9ea8f38656b10f290a3aae3284ae61798eae4
rami3l <rami3l@outlook.com>
2024-12-13 13:50:51 +0800

    refactor(neo-tree): extract `trash()`

--- lua/plugins/neo_tree.lua
+++ lua/plugins/neo_tree.lua
@@ -10,35 +10,34 @@ return {
       },
     })
     if vim.fn.executable("trash") == 1 then
+      local function trash(get_msg_and_paths)
+        return function(state, ...)
+          local msg, paths = get_msg_and_paths(state, ...)
+          require("neo-tree.ui.inputs").confirm(msg, function(confirmed)
+            if not confirmed then return end
+            vim.fn.system(vim.list_extend({ "trash" }, paths))
+            require("neo-tree.sources.manager").refresh(state.name)
+          end)
+        end
+      end
       opts = vim.tbl_deep_extend("force", opts, {
         filesystem = {
           window = { mappings = { D = "trash" } },
           commands = {
-            trash = function(state)
-              local inputs = require("neo-tree.ui.inputs")
+            trash = trash(function(state)
               local path = state.tree:get_node().path
               local _, name = require("neo-tree.utils").split_path(path)
               local msg = string.format("Are you sure you want to trash '%s'?", name)
-              inputs.confirm(msg, function(confirmed)
-                if not confirmed then return end
-                vim.fn.system { "trash", path }
-                require("neo-tree.sources.manager").refresh(state.name)
-              end)
-            end,
-            trash_visual = function(state, selected_nodes)
-              local inputs = require("neo-tree.ui.inputs")
-              local paths_to_delete = {}
-              for _, node_to_delete in pairs(selected_nodes) do
-                table.insert(paths_to_delete, node_to_delete.path)
+              return msg, { path }
+            end),
+            trash_visual = trash(function(_, nodes)
+              local paths = {}
+              for _, node in pairs(nodes) do
+                table.insert(paths, node.path)
               end
-              local msg =
-                string.format("Are you sure you want to trash %i items?", #paths_to_delete)
-              inputs.confirm(msg, function(confirmed)
-                if not confirmed then return end
-                vim.fn.system(vim.list_extend({ "trash" }, paths_to_delete))
-                require("neo-tree.sources.manager").refresh(state.name)
-              end)
-            end,
+              local msg = string.format("Are you sure you want to trash %i items?", #paths)
+              return msg, paths
+            end),
           },
         },
       })
