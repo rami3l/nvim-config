98c3013bf59663d69a97df23827257498dd360d9
rami3l <rami3l@outlook.com>
2025-02-12 10:10:42 +0800

    feat(ui): add `helpview-nvim`

--- lua/community.lua
+++ lua/community.lua
@@ -31,6 +31,7 @@ return {
   { import = "astrocommunity.motion.leap-nvim" },
   { import = "astrocommunity.motion.nvim-spider" },
   { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.neovim-lua-development.helpview-nvim" },
   { import = "astrocommunity.pack.clojure" },
   { import = "astrocommunity.pack.elixir-phoenix" },
   { import = "astrocommunity.pack.fish" },
