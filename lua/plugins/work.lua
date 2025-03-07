e95d55b1fb9806cc66e5e06cd61089d117d786cd
rami3l <rami3l@outlook.com>
2025-03-07 13:15:50 +0800

    feat(work): add work-specific plugins

---
+++ lua/plugins/work.lua
@@ -0,0 +1,25 @@
+---@type LazySpec
+return {
+  { import = "astrocommunity.pack.moonbit" },
+  {
+    "moonbit-community/moonbit.nvim",
+    -- dir = "<path>",
+  },
+
+  { import = "astrocommunity.pack.ocaml" },
+  { "tjdevries/ocaml.nvim", enabled = false },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed =
+        require("utils").list_remove_all(opts.ensure_installed, { "ocamllsp" })
+    end,
+  },
+  {
+    "AstroNvim/astrolsp",
+    optional = true,
+    ---@type AstroLSPOpts
+    opts = { servers = { "ocamllsp" } },
+  },
+}
