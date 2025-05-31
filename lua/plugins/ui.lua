a38f2d9bb5d465c2362216901824d0a63fe5696f
rami3l <rami3l@outlook.com>
2025-05-31 20:30:07 +0800

    feat(ui): update default dark theme

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,6 +1,6 @@
 local themes = {
   light = "github-monochrome-rosepine-dawn",
-  dark = "github-monochrome-rosepine",
+  dark = "kanagawa",
 }
 
 ---@type LazySpec
@@ -16,10 +16,15 @@ return {
   },
 
   -- Theming
-  { "Shatur/neovim-ayu", lazy = true },
   { "idr4n/github-monochrome.nvim", lazy = true },
-  { "mcchrish/zenbones.nvim", lazy = true, dependencies = { "rktjmp/lush.nvim" } },
-  { "nyoom-engineering/oxocarbon.nvim", lazy = true },
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = true,
+    opts = {
+      dimInactive = true,
+      background = { light = "lotus", dark = "wave" },
+    },
+  },
   {
     "catppuccin/nvim",
     name = "catppuccin",
@@ -28,13 +33,17 @@ return {
       background = { light = "latte", dark = "mocha" },
     },
   },
-  {
-    "rmehri01/onenord.nvim",
-    lazy = true,
-    opts = {
-      styles = { comments = "italic" },
-    },
-  },
+
+  -- { "Shatur/neovim-ayu", lazy = true },
+  -- { "mcchrish/zenbones.nvim", lazy = true, dependencies = { "rktjmp/lush.nvim" } },
+  -- { "nyoom-engineering/oxocarbon.nvim", lazy = true },
+  -- {
+  --   "rmehri01/onenord.nvim",
+  --   lazy = true,
+  --   opts = {
+  --     styles = { comments = "italic" },
+  --   },
+  -- },
 
   {
     "f-person/auto-dark-mode.nvim",
