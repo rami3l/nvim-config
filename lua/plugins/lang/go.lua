c301cb3f551bee43c8d5d5148abf22e5276c6658
rami3l <rami3l@outlook.com>
2025-06-16 16:54:23 +0800

    fix(lang/go): install `golangci-lint-langserver`

--- lua/plugins/lang/go.lua
+++ lua/plugins/lang/go.lua
@@ -5,6 +5,7 @@ return {
     opts = function(_, opts)
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
         "golangci-lint",
+        "golangci-lint-langserver",
       })
     end,
   },
