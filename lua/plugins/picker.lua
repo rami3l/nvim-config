d953c1ff1fe850c3d9ce4bc5c2f8d362066d4342
rami3l <rami3l@outlook.com>
2025-11-05 21:41:46 +0100

    feat(ide/picker): add hotkey for GitHub issue/PR pickers

--- lua/plugins/picker.lua
+++ lua/plugins/picker.lua
@@ -39,5 +39,27 @@ return {
       function() require("snacks.picker").lsp_incoming_calls() end,
       desc = "Find incoming calls",
     },
+
+    -- GitHub issues and PRs
+    {
+      "<leader>gi",
+      function() require("snacks.picker").gh_issue() end,
+      desc = "GitHub Issues (open)",
+    },
+    {
+      "<leader>gI",
+      function() require("snacks.picker").gh_issue { state = "all" } end,
+      desc = "GitHub Issues (all)",
+    },
+    {
+      "<leader>gp",
+      function() require("snacks.picker").gh_pr() end,
+      desc = "GitHub Pull Requests (open)",
+    },
+    {
+      "<leader>gP",
+      function() require("snacks.picker").gh_pr { state = "all" } end,
+      desc = "GitHub Pull Requests (all)",
+    },
   },
 }
