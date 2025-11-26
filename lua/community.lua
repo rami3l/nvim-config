396f6d801b86c9a74195534a66bf19d9361bff80
rami3l <rami3l@outlook.com>
2025-11-26 14:19:20 +0100

    feat(quickfix): add `quicker` for qflist enhancement

--- lua/community.lua
+++ lua/community.lua
@@ -45,6 +45,7 @@ return {
   { import = "astrocommunity.pack.vue" },
   { import = "astrocommunity.pack.yaml" },
   { import = "astrocommunity.pack.zig" },
+  { import = "astrocommunity.quickfix.quicker-nvim" },
   { import = "astrocommunity.recipes.picker-lsp-mappings" },
   { import = "astrocommunity.recipes.vscode-icons" },
   { import = "astrocommunity.search.grug-far-nvim" },
