7cc64489b84c58e0620e9eb0bde32a663445078b
rami3l <rami3l@outlook.com>
2025-12-29 15:22:33 +0100

    refactor(lang/zig): delegate `zls`' installation to pack

--- lua/plugins/lang/zig.lua
+++ lua/plugins/lang/zig.lua
@@ -1,14 +1,6 @@
 ---@type LazySpec
 return {
   {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    opts = function(_, opts)
-      opts.ensure_installed =
-        require("astrocore").list_insert_unique(opts.ensure_installed, { "zls" })
-    end,
-  },
-
-  {
     "AstroNvim/astrolsp",
     ---@type AstroLSPOpts
     ---@diagnostic disable: missing-fields
