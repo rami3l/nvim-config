4cada3425d7a01506e36192ed82786e3a0000940
rami3l <rami3l@outlook.com>
2026-01-03 16:46:27 +0100

    feat(lazy_setup): bump `AstroNvim` to v6

---
+++ lua/plugins/_beta.lua
@@ -0,0 +1,8 @@
+-- TODO: Remove this when AstroNvim v6 is officially released
+
+---@type LazySpec
+return {
+  { "AstroNvim/astrocore", version = false, branch = "v3" },
+  { "AstroNvim/astrolsp", version = false, branch = "v4" },
+  { "AstroNvim/astroui", version = false, branch = "v4" },
+}
