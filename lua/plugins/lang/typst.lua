9bbdb8783573c3f2d1045db64524628197de0325
rami3l <rami3l@outlook.com>
2026-03-04 10:16:49 +0100

    feat(lang/typst): add `ltex-ls-plus` for typo checking

--- lua/plugins/lang/typst.lua
+++ lua/plugins/lang/typst.lua
@@ -35,4 +35,12 @@ return {
       },
     },
   },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed =
+        require("astrocore").list_insert_unique(opts.ensure_installed, { "ltex-ls-plus" })
+    end,
+  },
 }
