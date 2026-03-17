11ed1e0964bb6d36b1b504008f699a4f16b0d90c
rami3l <rami3l@outlook.com>
2026-03-17 09:24:20 +0100

    refactor(lang/rust): get rid of `rustaceanvim`

--- lua/plugins/lang/rust.lua
+++ lua/plugins/lang/rust.lua
@@ -1,13 +1,42 @@
 ---@type LazySpec
 return {
+  -- Ported from https://github.com/AstroNvim/astrocommunity/blob/58639523ef0913005194235ebb66dc84e6b6ee05/lua/astrocommunity/pack/rust/init.lua.
   {
-    "mrcjkb/rustaceanvim",
-    ---@module "rustaceanvim"
-    ---@type rustaceanvim.Opts
+    "vxpm/ferris.nvim",
+    lazy = true,
+    init = function()
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("lazy_ferris", { clear = true }),
+        desc = "Lazy load Ferris",
+        once = true,
+        callback = function(args)
+          if
+            vim.lsp.get_client_by_id(args.data.client_id).name == "rust_analyzer"
+            and require("ferris.private.config").opts.create_commands
+          then
+            require("ferris").create_commands(args.buf)
+          end
+        end,
+      })
+    end,
+  },
+  {
+    "Saecki/crates.nvim",
+    event = { "BufRead Cargo.toml" },
     opts = {
-      server = { load_vscode_settings = true },
+      completion = {
+        crates = { enabled = true },
+      },
+      lsp = {
+        enabled = true,
+        on_attach = function(...) require("astrolsp").on_attach(...) end,
+        actions = true,
+        completion = true,
+        hover = true,
+      },
     },
   },
+
   {
     "Olical/conjure",
     dependencies = {
@@ -25,6 +54,26 @@ return {
     end,
   },
 
+  {
+    "AstroNvim/astrolsp",
+    dependencies = {
+      { "mrjones2014/codesettings.nvim", opts = {} },
+    },
+    ---@type AstroLSPOpts
+    ---@diagnostic disable: missing-fields
+    opts = {
+      config = {
+        rust_analyzer = {
+          before_init = function(_, cfg)
+            require("codesettings")
+              .loader()
+              :root_dir(cfg.root_dir)
+              :with_local_settings(cfg.name, cfg)
+          end,
+        },
+      },
+    },
+  },
   {
     "AstroNvim/astrolsp",
     ---@type AstroLSPOpts
