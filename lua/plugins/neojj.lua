b2e06925f629dedbf9b3108dd9f93fdbf4e1d8d1
rami3l <rami3l@outlook.com>
2026-04-25 22:25:03 +0200

    fix(jj/neojj): stop using fork following upstreamed fix

--- lua/plugins/neojj.lua
+++ lua/plugins/neojj.lua
@@ -1,11 +1,6 @@
 ---@type LazySpec
 return {
   "NicholasZolton/neojj",
-
-  -- HACK: Use my fork until the `squash` UI fix is merged.
-  url = "https://github.com/rami3l/neojj",
-  branch = "fix/squash-pty",
-
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
