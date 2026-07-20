a054fcccd6a9e46f3e05dbca2dc14d972f3f0e30
rami3l <rami3l@outlook.com>
2026-07-20 11:02:07 +0200

    hack(lsp): avoid drifting of the current line in insert mode

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,7 +7,7 @@ return {
   opts = {
     features = {
       autoformat = true,
-      codelens = true,
+      codelens = false,
       inlay_hints = true,
       lsp_handlers = true,
       semantic_tokens = true,
