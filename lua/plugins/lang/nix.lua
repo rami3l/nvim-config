6331761026ccc4ae5662d6a66d79595e80637be6
rami3l <rami3l@outlook.com>
2026-09-26 22:07:32 +0200

    feat(lang/nix): remove tool installation logic

--- lua/plugins/lang/nix.lua
+++ lua/plugins/lang/nix.lua
@@ -1,15 +1,6 @@
 ---@type LazySpec
 return {
   {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    optional = true,
-    opts = function(_, opts)
-      opts.ensure_installed =
-        require("astrocore").list_insert_unique(opts.ensure_installed, { "alejandra" })
-    end,
-  },
-
-  {
     "AstroNvim/astrolsp",
     ---@type AstroLSPOpts
     ---@diagnostic disable: missing-fields
